83c0b8a522d8d0ca142c7f2357ea06637f4ef5c3
Johan Günther Krogevoll <johan@krogevoll.com>
2024-04-19 22:07:41 +0200

    added lvim config

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,12 @@
+require('nvim-treesitter.install').compilers = { 'gcc' }
+
+reload('user.plugins')
+reload('user.colorscheme')
+reload('user.options')
+reload('user.keymaps')
+reload('user.which-key')
+reload('user.statusline')
+reload('user.lsp')
+reload('user.dashboard')
+-- reload('user.lsp-status')
+reload('user.autocmds')
