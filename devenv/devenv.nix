339b129cd0b6895d08f0cebbe12c06d2d1a34737
Johan Günther Krogevoll <johan@krogevoll.com>
2024-04-25 15:05:01 +0200

    cleanup

--- devenv/devenv.nix
+++ devenv/devenv.nix
@@ -1,10 +1,6 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
   packages = with pkgs; [
           bash-completion
           bashInteractive
@@ -67,32 +63,19 @@
           zsh
 ];
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hrello from $GREET";
+  scripts.hello.exec = "Devenv is ready!";
 
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
 
   dotenv.enable = true;
+
   # https://devenv.sh/languages/
   languages.nix.enable = true;
   languages.javascript.enable = true;
   languages.typescript.enable = true;
-  # languages.terraform.enable = true;
+  languages.terraform.enable = true;
 
   # https://devenv.sh/services/
   # services.postgres.enable = true;
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
