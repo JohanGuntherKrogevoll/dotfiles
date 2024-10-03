443be1fbeadd020abf711271b9f71679dbdb591b
Johan Günther Krogevoll <johan@krogevoll.com>
2024-10-03 20:38:31 +0200

    nvim: let example.lua be as it was originally and extract lsp stuff to own file

--- lazyvim/.config/nvim/lua/plugins/example.lua
+++ lazyvim/.config/nvim/lua/plugins/example.lua
@@ -1,53 +1,7 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 if true then
-  return {
-    {
-      "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = {
-          "stylua",
-          "shellcheck",
-          "shfmt",
-          "flake8",
-          "biome"
-        },
-      },
-    },
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        "jose-elias-alvarez/typescript.nvim",
-        init = function()
-          require("lazyvim.util").lsp.on_attach(function(_, buffer)
-            -- stylua: ignore
-            vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-            vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-          end)
-        end,
-      },
-      ---@class PluginLspOpts
-      opts = {
-        ---@type lspconfig.options
-        servers = {
-          -- tsserver will be automatically installed with mason and loaded with lspconfig
-          tsserver = {},
-        },
-        -- you can do any additional lsp server setup here
-        -- return true if you don't want this server to be setup with lspconfig
-        ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-        setup = {
-          -- example to setup with typescript.nvim
-          tsserver = function(_, opts)
-            require("typescript").setup({ server = opts })
-            return true
-          end,
-          -- Specify * to use this function as a fallback for any server
-          -- ["*"] = function(server, opts) end,
-        },
-      },
-    }
-  }
+  return {}
 end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
@@ -76,7 +30,7 @@ return {
   },
 
   -- disable trouble
-  { "folke/trouble.nvim",      enabled = false },
+  { "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
